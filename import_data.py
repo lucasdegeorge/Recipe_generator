efd4f078cc21dcb46e92cb4ffafc625575afd245
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2022-04-14 09:48:04 +0200

    Create import_data.py

---
+++ import_data.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 14 09:14:38 2022
+
+@author: lucas
+"""
+
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from math import * 
+
