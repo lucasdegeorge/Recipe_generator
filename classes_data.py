45bd334f60199ea826a480ee16383ad2e8128042
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2022-04-14 09:07:13 +0200

    Update classes_data.py

--- classes_data.py
+++ classes_data.py
@@ -6,28 +6,28 @@ from math import *
 class user:
     ''' 
     health is a dict with data about weight, height, sex, body_fat, ...
-    food is a np.array whose lines contain data (quantity, expiry_date, fondness) on a specific food
+    food is a np.array whose lines contain data (name, quantity, expiry_date, fondness) on a specific food
     '''
     def __init__(self,health, coord, budget, food):
         self.health_data = health
         self.adress = coord
         self.budget = budget
-        self.fidge = pd.DataFrame(data=aliments, columns=["quantity","expiry_date","fondness"])
+        self.fidge = pd.DataFrame(data=food, columns=["name","quantity","expiry_date","fondness"])
 
 
 class shop:
     ''' 
-    food is a np.array whose lines contain data (quantity, price, expiry_date) on a specific food 
+    food is a np.array whose lines contain data (name, quantity, price, expiry_date) on a specific food 
     '''
     def __init__(self, food, coord):
-        self.stocks = pd.DataFrame(data=food, columns=["quantity","price","expiry_date"])
+        self.stocks = pd.DataFrame(data=food, columns=["name","quantity","price","expiry_date"])
         self.hours = []
         self.adress = coord
 
 
 class recipe:
     def __init__(self, food, prep_time):
-        self.ingredient = pd.DataFrame(data=food, columns=["quantity"])
+        self.ingredient = pd.DataFrame(data=food, columns=["name","quantity"])
         self.prep_time = prep_time
        
 
@@ -36,9 +36,14 @@ class errand:
     shpping_list is a np.array whose lines contain data (quantity, price) on a specific food 
     '''
     def __init__(self,shopping_list):
-        self.needs = pd.DataFrame(data=shpping_list, columns=["quantity"])
+        self.needs = pd.DataFrame(data=shpping_list, columns=["name","quantity"])
         
-    def price(self, nb_shop):
-        
-        
-        
+    def price(self, store):
+        p = 0
+        for i in self.needs.index:
+            df = store.stocks.loc[store.stocks["name"] == self.needs[i] ]
+            
+        return df
+
+
+
