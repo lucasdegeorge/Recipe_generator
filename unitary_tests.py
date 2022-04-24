7b6f152b8d42a61cc15ba312e8c1d05f37c580a0
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2022-04-24 12:14:46 +0200

    Update unitary_tests.py

--- unitary_tests.py
+++ unitary_tests.py
@@ -9,6 +9,8 @@ def unitary_tests():
     print(ut_errand_price1())
     print(ut_errand_price2())
     print(ut_errand_price3())
+    print(ut_recipe_food_needed1())
+    print(ut_recipe_food_needed2())
     
 
 def ut_errand_price1():
@@ -29,5 +31,21 @@ def ut_errand_price3():
         return False
     except ProductNotAvailable:
         return True
+    
+def ut_recipe_food_needed1():
+    recipe_test = recipe(df_recipes["ingredients"][0], df_recipes["prep_time"][0],df_recipes["guests"][0] )
+    user_test = user(health = {}, coord = 0, budget = 100, 
+                     fridge = pd.DataFrame( {"name" : ["pasta","riz"], "quantity" : [10,3]}) ) 
+    res = pd.DataFrame({ "name" : ["pasta", "tomato"],
+                            "quantity" : [140, 2] })
+    return res.equals(recipe_test.food_needed(user_test))
+
+def ut_recipe_food_needed2():
+    recipe_test = recipe(df_recipes["ingredients"][0], df_recipes["prep_time"][0],df_recipes["guests"][0] )
+    user_test = user(health = {}, coord = 0, budget = 100, 
+                     fridge = pd.DataFrame( {"name" : ["pasta","riz"], "quantity" : [10,3]}) ) 
+    res = pd.DataFrame({ "name" : ["pasta", "tomato"],
+                            "quantity" : [140, 2] })
+    return res.equals(recipe_test.food_needed(user_test))
 
 unitary_tests()
