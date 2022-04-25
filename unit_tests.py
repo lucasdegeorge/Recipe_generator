30a1eae593f9e3901c5a7f973aef15cad1ed64f2
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2022-04-25 13:14:56 +0200

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -6,11 +6,18 @@ Created on Tue Apr 19 17:44:15 2022
 """
 
 def unit_tests():
+    print("errand.price")
     print(ut_errand_price1())
     print(ut_errand_price2())
     print(ut_errand_price3())
+    print("recipe.food_needed")
     print(ut_recipe_food_needed1())
     print(ut_recipe_food_needed2())
+    print("recipe.best_price")
+    print(ut_recipe_best_price1())
+    print(ut_recipe_best_price2())
+    print(ut_recipe_best_price3())
+    
     
 
 def ut_errand_price1():
@@ -47,6 +54,58 @@ def ut_recipe_food_needed2():
     res = pd.DataFrame({ "name" : ["pasta", "rice"],
                             "quantity" : [90, 97] })
     return res.equals(recipe_test.food_needed(user_test))
+
+def ut_recipe_best_price1():
+    recipe_test = recipe(df_recipes["ingredients"][0], df_recipes["prep_time"][0],df_recipes["guests"][0] )
+    user_test = user(health = {}, coord = 0, budget = 100, 
+                     fridge = pd.DataFrame( {"name" : ["pasta","rice"], "quantity" : [10,3]}) ) 
+    store_test1 = shop(pd.DataFrame( {"name" : ["pasta","rice","tomato"],
+                                      "quantity" : [150,3,2],
+                                      "price" : [1, 0.75, 0.4],
+                                      "expiry_date" : ["","",""] }  )
+                                   , 0)
+    store_test2 = shop(pd.DataFrame( {"name" : ["pasta","rice","tomato"],
+                                      "quantity" : [210,12,6],
+                                      "price" : [0.9, 1.25, 0.6],
+                                      "expiry_date" : ["","",""] }  ) 
+                                   , 0)
+    return recipe_test.best_price(user_test, [], [store_test1,store_test2]) == (1, 127.2)
+
+def ut_recipe_best_price2():
+    recipe_test = recipe(df_recipes["ingredients"][0], df_recipes["prep_time"][0],df_recipes["guests"][0] )
+    user_test = user(health = {}, coord = 0, budget = 100, 
+                     fridge = pd.DataFrame( {"name" : ["pasta","rice"], "quantity" : [10,3]}) ) 
+    store_test1 = shop(pd.DataFrame( {"name" : ["pasta","rice","tomato"],
+                                      "quantity" : [150,3,2],
+                                      "price" : [1, 0.75, 0.4],
+                                      "expiry_date" : ["","",""] }  )
+                                   , 0)
+    store_test2 = shop(pd.DataFrame( {"name" : ["pasta","rice","tomato"],
+                                      "quantity" : [110,12,6],
+                                      "price" : [0.9, 1.25, 0.6],
+                                      "expiry_date" : ["","",""] }  ) 
+                                   , 0)
+    return recipe_test.best_price(user_test, [], [store_test1,store_test2]) == (0,140.8)
+
+def ut_recipe_best_price3():
+    recipe_test = recipe(df_recipes["ingredients"][0], df_recipes["prep_time"][0],df_recipes["guests"][0] )
+    user_test = user(health = {}, coord = 0, budget = 100, 
+                     fridge = pd.DataFrame( {"name" : ["pasta","rice"], "quantity" : [10,3]}) ) 
+    store_test1 = shop(pd.DataFrame( {"name" : ["pasta","rice","tomato"],
+                                      "quantity" : [120,3,2],
+                                      "price" : [1, 0.75, 0.4],
+                                      "expiry_date" : ["","",""] }  )
+                                   , 0)
+    store_test2 = shop(pd.DataFrame( {"name" : ["pasta","rice","tomato"],
+                                      "quantity" : [110,12,6],
+                                      "price" : [0.9, 1.25, 0.6],
+                                      "expiry_date" : ["","",""] }  ) 
+                                   , 0)
+    try:
+        recipe_test.best_price(user_test, [], [store_test1,store_test2])
+        return False
+    except NoWhereToBuy:
+        return True
 
 unit_tests()
 
