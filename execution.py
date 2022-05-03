86f75c190b225678c86785c682616404ff9bda07
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2022-05-03 09:27:22 +0200

    Update execution.py

--- execution.py
+++ execution.py
@@ -4,13 +4,24 @@ Created on Tue May  3 09:20:00 2022
 
 @author: lucas
 """
-
+### Librairies
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
 from math import *
 import random as rd
-import googlemaps as gm 
+#import googlemaps as gm 
 import pandas as pd
-import geopy as gp
+#import geopy as gp
 import time 
+
+### User settings 
+
+
+
+### Ingredients data 
+from import_data.py import *
+
+
+
+
