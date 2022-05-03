89376fafa562bc710b6909a2ce86e3220df782ac
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2022-05-03 09:21:06 +0200

    Create execution.py

---
+++ execution.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 09:20:00 2022
+
+@author: lucas
+"""
+
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+import random as rd
+import googlemaps as gm 
+import pandas as pd
+import geopy as gp
+import time 
