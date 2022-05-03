3f41bcd594d421c5a94d2193a56e11f9d47cb60d
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2022-05-03 08:30:15 +0200

    Update geolocalization.py

--- geolocalization.py
+++ geolocalization.py
@@ -87,5 +87,70 @@ print(travel_informations)
 
 
 
+## Function of geolocalization 
+
+def find_supermarkets(adress, radius = 4e3):
+  '''
+  return a list of supermarkets with the name, the distance and the estimated travel time
+  '''
+  
+  # We determine the localisation of the user
+  geolocator = gp.geocoders.Nominatim(user_agent="my_request")  
+  user_location = geolocator.geocode(adress)                          
+  user_coords = (user_location.latitude, user_location.longitude)
+  
+  # We determine the nearest supermarkets
+  grocery_request = map_client.places_nearby(location = user_coords,                                            
+                                             keyword = search_string,                                            
+                                             name = 'Supermarket',                                               
+                                             radius = distance)
+  grocery_stores_list.extend(grocery_request.get('results'))
+  next_page_token = grocery_request.get('next_page_token')
+  
+  while next_page_token:
+    time.sleep(5)
+    grocery_request = map_client.places_nearby(location = user_coords,
+                                               keyword = search_string,
+                                               name = 'Supermarket',
+                                               radius = distance,
+                                               page_token = next_page_token)
+    grocery_stores_list.extend(grocery_request.get('results'))
+    next_page_token = grocery_request.get('next_page_token') 
+
+  # The list is converted as a dataframe
+  df = pd.DataFrame(grocery_stores_list)
+  
+  grocery_names = df.filter(['name'])
+  grocery_stores = df.filter(['geometry'])
+  
+  n = len(grocery_names)
+  
+  grocery_stores_locations = [[grocery_names['name'][i],
+                               grocery_stores['geometry'][i]['location']['lat'],
+                               grocery_stores['geometry'][i]['location']['lng']] for i in range(n)]
+  
+  grocery_stores_coords = [(grocery_stores_locations[i][1],
+                            grocery_stores_locations[i][2]) for i in range(n)]
+                            
+
+  # We determine the distance and the travel time of each supermarket
+  trip_list = []
+
+  for i in range(n): 
+    trip_list.append(map_client.distance_matrix(origins = user_coords,
+                                                destinations = grocery_stores_coords[i]))
+  
+  trip_df = pd.DataFrame(trip_list) 
+  
+  travel_informations = [(grocery_names['name'][i], 
+                          trip_df['rows'][i][0]['elements'][0]['distance']['text'],
+                          trip_df['rows'][i][0]['elements'][0]['duration']['text']) for i in range(n)]
+                          
+  return travel_informations
+  
+  
+# Test : 
+
+print(find_supermarkets('27 Boulevard Thomas Gobert, Palaiseau 91120'))
 
 
